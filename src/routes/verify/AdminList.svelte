168002977325b8ac606ef73fdc87132cc4e83bcd
Mayur Nagare <mayurnagare@Mayurs-MacBook-Pro.local>
2022-08-17 11:59:15 +0530

    added listing of verifies, verifytiers and verifyTier Report

---
+++ src/routes/verify/AdminList.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { formatAddress } from "$src/utils";
+
+  export let admins, heading;
+</script>
+
+<div class="w-1/3">
+  <span class="text-white">{heading}</span>
+  {#each admins as admin}
+    <div class="text-gray-400 flex flex-col mb-4">
+      <span>Address: {formatAddress(admin.address)}</span>
+      <!-- <span>Roles: {admin.roles}</span> -->
+    </div>
+  {/each}
+</div>
