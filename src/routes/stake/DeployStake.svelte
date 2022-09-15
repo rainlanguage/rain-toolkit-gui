07de06c989d48050edcdeae22b321bcd0221c314
Mayur Nagare <102811097+nagaremayur@users.noreply.github.com>
2022-09-15 11:38:18 +0530

    Update DeployStake.svelte

--- src/routes/stake/DeployStake.svelte
+++ src/routes/stake/DeployStake.svelte
@@ -39,8 +39,6 @@
         .div(BigNumber.from("1".padEnd(erc20Info.decimals, "0")));
     }
     
-    console.log("stakeArgs", erc20Address, erc20Info.decimals, InitialRatio.toString());
-    
     const stakeArgs: StakeDeployArgs = {
       token: erc20Address,
       initialRatio: InitialRatio,
