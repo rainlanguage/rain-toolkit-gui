784a8d747426d1b954639b6275a20b4c580637f8
Mayur Nagare <mayurnagare@Mayurs-MacBook-Pro.local>
2022-11-14 21:13:25 +0530

    withdraw modal fixes

--- src/routes/order-book/WithdrawModal.svelte
+++ src/routes/order-book/WithdrawModal.svelte
@@ -136,7 +136,7 @@
       {/if}
     
       <span>Confirm your withdraw.</span>
-      <Button bRadius="rounded-full" variant="bg-orange-400" on:click={withdraw}>Confirm</Button>
+      <Button bRadius="rounded-full" variant="bg-orange-400" disabled={!priceConfirmed} on:click={withdraw}>Confirm</Button>
     {/if}
 
     {#if activeStep == WithdrawSteps.Complete}
