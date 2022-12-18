425f7f1a766296c7bd1ff2117942395df91abe52
Mayur Nagare <cryptodeveloper22@gmail.com>
2022-12-18 14:14:23 +0530

    svg removed

--- src/routes/assets.ts
+++ src/routes/assets.ts
@@ -2,6 +2,5 @@ import sloshy_comic from '$src/assets/sloshy_comic.webp'
 import not_found from '$src/assets/404.webp'
 import polygon from '$src/assets/polygon.svg'
 import ethereum from '$src/assets/ethereum.svg'
-import sloshy_background from '$src/assets/sloshy_background.svg'
 
-export const img = { sloshy_comic, polygon, ethereum, sloshy_background, not_found }
+export const img = { sloshy_comic, polygon, ethereum, not_found }
